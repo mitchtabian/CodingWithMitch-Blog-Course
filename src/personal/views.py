c16789eaf3aae7b163163f4207ce6c1bf560831d
Mitch Tabian <mitch@tabian.ca>
2019-06-16 15:39:22 -0700

    Passing Variables to a Template with Django

--- src/personal/views.py
+++ src/personal/views.py
@@ -3,5 +3,20 @@ from django.shortcuts import render
 # Create your views here.
 
 def home_screen_view(request):
-	print(request.headers)
-	return render(request, "personal/home.html", {})
+	context = {}
+	# context['some_string'] = "this is some string that I'm passing to the view"
+
+	# context = {
+	# 	'some_string': "this is some string that I'm passing to the view"
+	# }
+
+	# context['some_number'] = 3151351
+
+	list_of_values = []
+	list_of_values.append("first entry")
+	list_of_values.append("second entry")
+	list_of_values.append("third entry")
+	list_of_values.append("fourth entry")
+	list_of_values.append("fifth entry")
+	context['list_of_values'] = list_of_values
+	return render(request, "personal/home.html", context)
