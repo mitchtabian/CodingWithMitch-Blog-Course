413d3bf9c62cc9030c7d340276b8bb1ed7da3e5c
Mitch Tabian <mitch@tabian.ca>
2019-06-18 09:51:52 -0700

    Customizing Django Admin

--- src/personal/admin.py
+++ src/personal/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
-from personal.models import Question
+from django.contrib.auth.admin import UserAdmin
+from account.models import Account
 
-# Register your models here.
 
-admin.site.register(Question)
+class AccountAdmin(UserAdmin):
+	list_display = ('email','username','date_joined', 'last_login', 'is_admin','is_staff')
+	search_fields = ('email','username',)
+	readonly_fields=('date_joined', 'last_login')
+
+	filter_horizontal = ()
+	list_filter = ()
+	fieldsets = ()
+
+
+admin.site.register(Account, AccountAdmin)
+
+
+
--- src/personal/views.py
+++ src/personal/views.py
@@ -1,11 +1,5 @@
 from django.shortcuts import render
-from personal.models import Question
 
 
-def home_screen_view(request):
-	
-	context = {}
-	questions = Question.objects.all()
-	context['questions'] = questions
 
-	return render(request, "personal/home.html", context)
+
