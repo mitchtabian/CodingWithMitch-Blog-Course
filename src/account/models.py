fe526ba12c01bbb29f47b13fc863ba0f0c27c176
Mitch Tabian <mitch@tabian.ca>
2019-06-17 17:15:51 -0700

    Creating a Custom User Model (Django)

--- src/account/models.py
+++ src/account/models.py
@@ -3,20 +3,20 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 
 class MyAccountManager(BaseUserManager):
-    def create_user(self, email, username, password=None):
-        if not email:
-            raise ValueError('Users must have an email address')
-        if not username:
-            raise ValueError('Users must have a username')
+	def create_user(self, email, username, password=None):
+		if not email:
+			raise ValueError('Users must have an email address')
+		if not username:
+			raise ValueError('Users must have a username')
 
-        user = self.model(
-            email=self.normalize_email(email),
-            username=username,
-        )
+		user = self.model(
+			email=self.normalize_email(email),
+			username=username,
+		)
 
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
+		user.set_password(password)
+		user.save(using=self._db)
+		return user
 
 	def create_superuser(self, email, username, password):
 		user = self.create_user(
