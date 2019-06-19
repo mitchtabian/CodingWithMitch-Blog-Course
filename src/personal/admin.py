971f542412f9b53613af25fba75580bd820c30c1
Mitch Tabian <mitch@tabian.ca>
2019-06-19 11:26:44 -0700

    Customizing Django Admin (search and filtering)

--- src/personal/admin.py
+++ src/personal/admin.py
@@ -1,6 +1,2 @@
 from django.contrib import admin
-from personal.models import Question
 
-# Register your models here.
-
-admin.site.register(Question)
--- src/personal/views.py
+++ src/personal/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
-from personal.models import Question
 
+from account.models import Account
 
 def home_screen_view(request):
-	
 	context = {}
-	questions = Question.objects.all()
-	context['questions'] = questions
+	
+	accounts = Account.objects.all()
+	context['accounts'] = accounts
 
 	return render(request, "personal/home.html", context)
