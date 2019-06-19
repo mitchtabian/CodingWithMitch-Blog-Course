efc0e8bc519cfff78449d223eea504588773aa59
Mitch Tabian <mitch@tabian.ca>
2019-06-19 11:34:33 -0700

    Creating a Custom User Model (Django)

--- src/personal/models.py
+++ src/personal/models.py
@@ -1,22 +1,20 @@
-from django.db import models
-
-
+#The first element in each tuple is the value that will be stored in the database. The second element is displayed by the field’s form widget.
+#Tuple
 PRIORITY = [
-	("H", "High"),
-	("M", "Medium"),
-	("L", "Low"),
+	('L', 'Low'), #Tuple1
+	('M', 'Medium'), #Tuple2
+	('H', 'High'), #Tuple3
 ]
 
 class Question(models.Model):
-	title 					= models.CharField(max_length=60)
+	title		 			= models.CharField(max_length=60)
 	question 				= models.TextField(max_length=400)
 	priority 				= models.CharField(max_length=1, choices=PRIORITY)
 
 	def __str__(self):
 		return self.title
 
+
 	class Meta:
-		verbose_name = "The Question"
-		verbose_name_plural = "Peoples Questions"
-
-
+		verbose_name = 'The Question'
+		verbose_name_plural = 'Questions from People'
