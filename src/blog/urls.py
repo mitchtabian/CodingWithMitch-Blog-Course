f25fb0f97be25123297737a5f9c7b02689a1b31f
Mitch Tabian <mitch@tabian.ca>
2019-06-21 20:11:35 -0700

    Updating Blog Posts using a Django Form (Images)

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from blog.views import (
 	create_blog_view,
 	detail_blog_view,
+	edit_blog_view,
 )
 
 app_name = 'blog'
@@ -9,6 +10,7 @@ app_name = 'blog'
 urlpatterns = [
     path('create/', create_blog_view, name="create"),
     path('<slug>/', detail_blog_view, name="detail"),
+    path('<slug>/edit/', edit_blog_view, name="edit"),
  ]
 
 
--- src/blog/views.py
+++ src/blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 
 from blog.models import BlogPost
-from blog.forms import CreateBlogPostForm
+from blog.forms import CreateBlogPostForm, UpdateBlogPostForm
 from account.models import Account
 
 
@@ -28,9 +28,39 @@ def create_blog_view(request):
 
 
 def detail_blog_view(request, slug):
-	
+
 	context = {}
 	blog_post = get_object_or_404(BlogPost, slug=slug)
 	context['blog_post'] = blog_post
 
 	return render(request, 'blog/detail_blog.html', context)
+
+
+
+
+def edit_blog_view(request, slug):
+	
+	context = {}
+	user = request.user
+	if not user.is_authenticated:
+		return redirect('must_authenticate')
+
+	blog_post = get_object_or_404(BlogPost, slug=slug)
+	if request.POST:
+		form = UpdateBlogPostForm(request.POST or None, request.FILES or None, instance=blog_post)
+		if form.is_valid():
+			obj = form.save(commit=False)
+			obj.save()
+			context['success_message'] = "Updated"
+			blog_post = obj
+	
+	form = UpdateBlogPostForm(
+			initial={
+					"title": blog_post.title, 
+					"body": blog_post.body,
+					"image": blog_post.image,
+				}
+			)
+	context['form'] = form
+
+	return render(request, 'blog/edit_blog.html', context)
