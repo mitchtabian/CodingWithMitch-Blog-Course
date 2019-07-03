1e737b170cdc5a2093fe9e9786fec17981128c2e
Mitch Tabian <mitch@tabian.ca>
2019-07-03 11:17:13 -0700

    Bug Fixes (Part 1)

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.db.models import Q
+from django.http import HttpResponse
 
 from blog.models import BlogPost
 from blog.forms import CreateBlogPostForm, UpdateBlogPostForm
@@ -17,20 +18,20 @@ def create_blog_view(request):
 	form = CreateBlogPostForm(request.POST or None, request.FILES or None)
 	if form.is_valid():
 		obj = form.save(commit=False)
-		author = Account.objects.filter(email=request.user.email).first()
+		author = Account.objects.filter(email=user.email).first()
 		obj.author = author
 		obj.save()
 		form = CreateBlogPostForm()
 
 	context['form'] = form
 
-	return render(request, 'blog/create_blog.html', context)
-
+	return render(request, "blog/create_blog.html", context)
 
 
 def detail_blog_view(request, slug):
 
 	context = {}
+
 	blog_post = get_object_or_404(BlogPost, slug=slug)
 	context['blog_post'] = blog_post
 
@@ -38,15 +39,19 @@ def detail_blog_view(request, slug):
 
 
 
-
 def edit_blog_view(request, slug):
-	
+
 	context = {}
+
 	user = request.user
 	if not user.is_authenticated:
-		return redirect('must_authenticate')
+		return redirect("must_authenticate")
 
 	blog_post = get_object_or_404(BlogPost, slug=slug)
+
+	if blog_post.author != user:
+		return HttpResponse('You are not the author of that post.')
+
 	if request.POST:
 		form = UpdateBlogPostForm(request.POST or None, request.FILES or None, instance=blog_post)
 		if form.is_valid():
@@ -54,32 +59,29 @@ def edit_blog_view(request, slug):
 			obj.save()
 			context['success_message'] = "Updated"
 			blog_post = obj
-	
+
 	form = UpdateBlogPostForm(
-			initial={
-					"title": blog_post.title, 
+			initial = {
+					"title": blog_post.title,
 					"body": blog_post.body,
 					"image": blog_post.image,
-				}
-			)
+			}
+		)
+
 	context['form'] = form
-
 	return render(request, 'blog/edit_blog.html', context)
-
-
 
 
 def get_blog_queryset(query=None):
 	queryset = []
-	queries = query.split(" ")
+	queries = query.split(" ") # python install 2019 = [python, install, 2019]
 	for q in queries:
 		posts = BlogPost.objects.filter(
-			Q(title__contains=q)|
-			Q(body__icontains=q)
+				Q(title__icontains=q) | 
+				Q(body__icontains=q)
 			).distinct()
+
 		for post in posts:
 			queryset.append(post)
 
-	# create unique set and then convert to list
-	return list(set(queryset)) 
-
+	return list(set(queryset))	
