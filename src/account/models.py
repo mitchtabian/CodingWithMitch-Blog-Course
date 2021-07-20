9af83824ece866257f583a10f89525bacc0ab12a
farkon00 <59134591+farkon00@users.noreply.github.com>
2021-07-20 19:26:30 +0300

    Update models.py in account, bug fix(lines 35 - 42)
    Tabs on this lines make errors, so I fix it.

--- src/account/models.py
+++ src/account/models.py
@@ -32,14 +32,14 @@ class MyAccountManager(BaseUserManager):
 
 
 class Account(AbstractBaseUser):
-	email 					= models.EmailField(verbose_name="email", max_length=60, unique=True)
-	username 				= models.CharField(max_length=30, unique=True)
-	date_joined				= models.DateTimeField(verbose_name='date joined', auto_now_add=True)
-	last_login				= models.DateTimeField(verbose_name='last login', auto_now=True)
-	is_admin				= models.BooleanField(default=False)
-	is_active				= models.BooleanField(default=True)
-	is_staff				= models.BooleanField(default=False)
-	is_superuser			= models.BooleanField(default=False)
+    email 					= models.EmailField(verbose_name="email", max_length=60, unique=True)
+    username 				= models.CharField(max_length=30, unique=True)
+    date_joined				= models.DateTimeField(verbose_name='date joined', auto_now_add=True)
+    last_login				= models.DateTimeField(verbose_name='last login', auto_now=True)
+    is_admin				= models.BooleanField(default=False)
+    is_active				= models.BooleanField(default=True)
+    is_staff				= models.BooleanField(default=False)
+    is_superuser			= models.BooleanField(default=False)
 
 
 	USERNAME_FIELD = 'email'
